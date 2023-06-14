311069307f04122c721d9c8b079ec02eb1e41fed
WTaroki <william.tanjaya@binus.ac.id>
2023-06-14 23:07:34 +0700

    feat(navbar) : added slider

--- vior/src/Navbar.jsx
+++ vior/src/Navbar.jsx
@@ -2,79 +2,138 @@ import LogoB from '../src/assets/VectorB.png'
 import 'boxicons';
 import { useState } from 'react';
 import { Link} from 'react-scroll'
-import Profile from '../src/assets/Profile.jpg'
+import Profile from '../src/assets/Profile.png'
 
 const Navbar = () => {
     const [MenuIcon,setMenuIcon] = useState(true);
-    const [SubMenu, setSubMenu] = useState(true);
-    const [SubMenu1, setSubMenu1] = useState(true);
-    const [SubMenu2, setSubMenu2] = useState(true);
-    const SubMenuList = [SubMenu,SubMenu1,SubMenu2]
-    const [Index, setIndex] = useState(true)
+    const [SubMenuList, setSubMenu] = useState([true,true,true]);
 
-    const MenuStyle = ['flex flex-col mx-3 px-6 mt-3 py-1 gap-y-2 border-gray-500 border-l-2 h-0 hidden','flex flex-col mx-3 px-6 mt-3 py-1 gap-y-2 border-gray-500 border-l-2 h-auto ']
+    const DropDown = (index) => {
+        const updatedList = [...SubMenuList];
+        updatedList[index] = !updatedList[index];
+        for (let i = 0; i < SubMenuList.length; i++) {
+            if (i !== index && !updatedList[i]) {
+                updatedList[i] = !updatedList[i];
+            }
+          }
+        setSubMenu(updatedList);
+    }
+    const MenuStyle = ['flex flex-col mx-7 px-5 my-1 py-2 gap-y-1 border-gray-400 border-l-2 h-0 hidden ','flex flex-col mx-7 px-5 my-1 py-2 gap-y-1 border-gray-400 border-l-2 h-auto ']
+    const NavStyle = ['fixed top-0 left-0 h-full w-[250px] bg-[#ffff4] pt-4 pb-2 shadow-md text-gray-600 px-2 transition-width duration-300 delay-400 overflow-hidden','fixed top-0 left-0 h-full w-[73px] bg-[#ffff4] pt-4 pb-2 shadow-md text-gray-600 px-2 transition-width duration-300']
+
     const NavListTop = [
-        {icon: <box-icon type='solid' name='grid-alt'></box-icon> ,name : "Home"        ,link:"/"},
-        {icon: <box-icon type='solid' name='grid-alt'></box-icon> ,name : "Products"    ,link:"/",Type:<box-icon name='chevron-down' onClick={() => setSubMenu(!SubMenu)}></box-icon>, Sub1: "New1", Sub2: "New2",Sub3 : "New4", Sub1Link:"/",Sub2Link:"/",Sub3Link:"/"},
-        {icon: <box-icon type='solid' name='grid-alt'></box-icon> ,name : "About"       ,link:"/",Type:<box-icon name='chevron-down' onClick={() => setSubMenu1(!SubMenu1)}></box-icon>, Sub1: "New1", Sub2: "New2",Sub3 : "New4", Sub1Link:"/",Sub2Link:"/",Sub3Link:"/"},
-        {icon: <box-icon type='solid' name='grid-alt'></box-icon> ,name : "Affiliate"   ,link:"/",Type:<box-icon name='chevron-down' onClick={() => setSubMenu2(!SubMenu2)}></box-icon>, Sub1: "New1", Sub2: "New2",Sub3 : "New4", Sub1Link:"/",Sub2Link:"/",Sub3Link:"/"},
-        {icon: <box-icon type='solid' name='grid-alt'></box-icon> ,name : "Interns"     ,link:"/"}
+    {   
+        icon: <box-icon type='solid' name='grid-alt'></box-icon> ,
+        name : "Home"        ,
+        link:"/"
+    },
+        {
+            icon: <box-icon name='buildings'></box-icon> ,
+            name : "Products"    ,
+            link:"/",
+            Type:true, 
+            Sub1: "Kost", 
+            Sub2: "Apartment",
+            Sub3 : "Rent House", 
+            Sub1Link:"/",
+            Sub2Link:"/",
+            Sub3Link:"/"
+        },
+        {
+            icon: <box-icon name='user-pin'></box-icon> ,
+            name : "About"       ,
+            link:"/",
+            Type:true, 
+            Sub1: "Instagram", 
+            Sub2: "Twitter",
+            Sub3 : "Facebook", 
+            Sub1Link:"/",
+            Sub2Link:"/",
+            Sub3Link:"/"},
+        {
+            icon: <box-icon type='solid' name='inbox'></box-icon> ,
+            name : "Resources"   ,
+            link:"/",
+            Type:true, 
+            Sub1: "Affiliate", 
+            Sub2: "Help Center",
+            Sub3 : "Contact",
+            Sub4:"Services", 
+            Sub1Link:"/",
+            Sub2Link:"/",
+            Sub3Link:"/",
+            Sub4Link: "/"},
+        {
+            icon: <box-icon name='category' ></box-icon> ,
+            name : "Categories"     ,
+            link:"/"
+        }
     ]
 
     return(
-        <nav className='fixed top-0 left-0 h-full w-[240px] bg-slate-300 py-4 px-3'>
+        <nav className={MenuIcon ? NavStyle[0] : NavStyle[1] }>
             <header className='flex flex-col relative h-full'>
                 <div className='flex justify-between'>
-                    <div className='flex flex-row items-center'>
-                            <box-icon type='solid' name='cube' size="lg"></box-icon>
-                            <h1 className='font-poppins text-4xl font-bold px-2'>Vior</h1>
+                    <div className='flex flex-row items-center px-2 py-2'>
+                        <box-icon type='solid' name='cube' size="lg"></box-icon>
+                        <h1 className='font-play text-4xl font-bold px-2 text-black'>{MenuIcon ? 'Vior' : ''}</h1>
                     </div>
                     <div onClick={() => setMenuIcon(!MenuIcon)}>
                         {MenuIcon ? <box-icon name='x'></box-icon> : <box-icon name='menu' ></box-icon>}
                     </div>
                 </div>
-                <div className='py-5 border-solid border-gray-500 border-b-2'>
-                    <input type="text" placeholder='Search' className='p-2 rounded-sm outline-slate-400 w-full'/>
+                <div className='mt-4 py-3 border-solid border-gray-300 border-b-2 px-1'>
+                    <div className='bg-gray-100 flex flex-row items-center px-3 py-3 rounded-sm border-slate-400 border-[1px] '>
+                        {
+                        MenuIcon ? 
+                        <box-icon name='search-alt-2' className="p-2"></box-icon> :
+                        <box-icon name='search-alt-2' animation='tada' className="p-2"></box-icon> 
+                        }
+                        {
+                        MenuIcon ? 
+                        <input type="text" placeholder='Search' className=' rounded-sm outline-none w-full bg-transparent text-black px-2'/> 
+                        : ''
+                        }
+                        
+                    </div>
                 </div>
-                <div className='flex flex-col justify-between h-full'>
-                    <div className='py-3 flex flex-col gap-y-4'>
+                <div className='flex flex-col justify-between h-full '>
+                    <div className='py-3 flex flex-col'>
                         {NavListTop.map((Nav, index) => (
-                            <div key={index} >
-                                <div className='flex flex-row justify-between items-center'>
-                                    <Link to={Nav.link} className='flex flex-row gap-x-4'>
+                            <div key={index} className=' hover:bg-slate-200 rounded-md transition ease-in-out duration-500 overflow-hidden py-1'>
+                                <div className='flex flex-row justify-between items-center py-3 px-4'>
+                                    <Link to={Nav.link} className='flex flex-row gap-x-4  '>
                                         {Nav.icon}
                                         <p>{Nav.name}</p>
                                     </Link>
-                                    {Nav.Type}
+                                    {Nav.Type? <box-icon name='chevron-down' onClick={() => DropDown(index-1)}></box-icon> : ''}
                                 </div>
                                 {
                                     MenuIcon?
                                     Nav.Sub1? 
                                     <div className={SubMenuList[index-1] ? MenuStyle[0] : MenuStyle[1]}>
-                                        <Link to={Nav.Sub1Link}>{Nav.Sub1}</Link>
-                                        <Link to={Nav.Sub2Link}>{Nav.Sub2}</Link>
-                                        <Link to={Nav.Sub3Link}>{Nav.Sub3}</Link>
+                                        <Link to={Nav.Sub1Link} className='hover:bg-slate-300 px-2 py-1 rounded-sm transition ease-in-out delay-200'>{Nav.Sub1}</Link>
+                                        <Link to={Nav.Sub2Link} className='hover:bg-slate-300 px-2 py-1 rounded-sm transition ease-in-out delay-200'>{Nav.Sub2}</Link>
+                                        <Link to={Nav.Sub3Link} className='hover:bg-slate-300 px-2 py-1 rounded-sm transition ease-in-out delay-200'>{Nav.Sub3}</Link>
+                                        <Link to={Nav.Sub4Link} className='hover:bg-slate-300 px-2 py-1 rounded-sm transition ease-in-out delay-200'>{Nav.Sub4}</Link>
                                     </div> : '' 
                                     : ''
                                 }
                             </div>
                         ))}
                     </div>
-                    <div className='flex flex-col'>
-                        <Link to='/' className='flex flex-row items-center gap-x-2 py-2'>
-                            <box-icon name='cog' ></box-icon> 
-                            <p>Setting</p>
-                        </Link>  
-                        <div className='flex flex-row gap-x-4 items-center border-gray-500 border-t-2 pt-3'>
+
+
+                        <div className='flex flex-row gap-x-4 items-center border-gray-300 border-t-2 pt-3 px-2 overflow-hidden'>
                             <img src={Profile} alt="" className='w-10 h-10 rounded-md'/>
-                            <div className='flex flex-col'>
+                            <div className='flex flex-col px-1'>
                                 <h1 className='text-sm font-bold'>Yayan</h1>
                                 <p className='text-xs'>yayan123@gmail.com</p>
                             </div>
                             <box-icon name='log-out' ></box-icon>
                         </div>
                     </div>
-                </div>
+
             </header>
         </nav>
     )
